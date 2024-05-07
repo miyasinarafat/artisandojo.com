9a6b7bee6da63615bef9e20beeaf33c3dd000670
miyasinarafat <miyasinarafat@gmail.com>
2024-05-07 22:22:38 +0600

    update cookie setting banner

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -30,7 +30,7 @@ export default function CookieBanner() {
 
     return (
 
-        <div className="pointer-events-none fixed inset-x-0 bottom-0 px-6 pb-6">
+        <div className={`pointer-events-none fixed inset-x-0 bottom-0 px-6 pb-6 ${cookieConsent != null ? "hidden" : "flex"}`}>
             <div className="pointer-events-auto max-w-xs rounded-xl bg-white p-6 shadow-lg ring-1 ring-gray-900/10">
                 <p className="text-sm leading-6 text-gray-900">
                     We value your privacy. We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking &quot;Allow Cookies&quot;, you consent to our use of cookies. See our{' '}
