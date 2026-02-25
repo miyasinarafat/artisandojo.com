999a4b38893a842263f50f66ecae9ca413f5e368
MD Iyasin Arafat <miyasinarafat@gmail.com>
2026-02-25 11:55:41 +0000

    update: under development

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,12 +22,26 @@ export const metadata = {
   },
 }
 
+function CommingSoon({ children, client, className }) {
+    return (
+         <div className="max-w-2xl text-center">
+            <h1 className="font-display text-4xl font-semibold tracking-tight text-neutral-950 sm:text-5xl">
+                We&apos;re building something great
+            </h1>
+            <p className="mt-4 text-lg text-neutral-600">
+                Our new site launches soon...
+            </p>
+      </div>
+    )
+}
+
 export default function Layout({ children }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
-        <RootLayout>{children}</RootLayout>
-        <CookieBanner/>
+        {/* <RootLayout>{children}</RootLayout>
+        <CookieBanner/> */}
+        <CommingSoon />
       </body>
     </html>
   )
